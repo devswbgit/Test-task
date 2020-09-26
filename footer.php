fb6d81c4e15fade89fc9103bd073368be3e889bf
devswbgit <devswbm@gmail.com>
2020-09-26 23:56:04 +0300

    fix footer

--- footer.php
+++ footer.php
@@ -3,22 +3,23 @@
     <div class="container">
         <div class="footer-inner">
 
-        <div class="logo">
-            <?php
-            if ( function_exists( 'the_custom_logo' ) ) {
-                the_custom_logo();
-            }
-            ?>
-        </div>
+            <div class="logo">
+                <?php
+                if ( function_exists( 'the_custom_logo' ) ) {
+                    the_custom_logo();
+                }
+                ?>
+            </div>
 
-        <?php if (is_active_sidebar('sidebar-primary')) {
-            dynamic_sidebar('footer-primary');
-        } ?>
-
-        <div class="footer-latest-post">
-            <?php if (is_active_sidebar('latest-posts')) {
-                dynamic_sidebar('latest-posts');
+            <?php if (is_active_sidebar('sidebar-primary')) {
+                dynamic_sidebar('footer-primary');
             } ?>
+
+            <?php if (is_active_sidebar('latest-posts')) { ?>
+            <div class="footer-latest-post">
+                <?php dynamic_sidebar('latest-posts'); ?>
+            </div>
+            <?php } ?>
         </div>
     </div>
     
